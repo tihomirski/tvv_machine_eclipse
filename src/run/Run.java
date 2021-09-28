376e61fe01f5de43c409d07dbd2593cc01211490
tihomirski <tivachkov@gmail.com>
2021-09-28 19:02:59 +0300

    spck git

--- src/run/Run.java
+++ src/run/Run.java
@@ -24,7 +24,7 @@ public class Run {
 //    	for (String key : System.getProperties()) {
 //    		System.out.println(key + " = " + System.getenv().get(key));
 //    	}
-//    	PC
+//    	spck editorrrrrrr
     	System.getProperties().list(System.out);
 		initFrame();
 //        System.out.println(generateSample());
